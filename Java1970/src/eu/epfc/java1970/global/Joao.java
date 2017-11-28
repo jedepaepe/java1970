baf55be342c3423d379efcabe68ac9086f999ddf
1310jocarvalho <1310jocarvalho@PC9792.domepfc.net>
2017-11-28 21:34:37 +0100

    Class Joao

---
+++ Java1970/src/eu/epfc/java1970/global/Joao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.epfc.java1970.global;
+
+/**
+ *
+ * @author 1310jocarvalho
+ */
+public class Joao {
+    public static void main(String[] args) {
+        System.out.println("Hello Joao !");
+    }
+}
