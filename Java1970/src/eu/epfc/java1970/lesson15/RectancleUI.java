7e331d071ae6c78287db89a4290d34aeb0d4ffb8
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2018-01-09 22:11:28 +0100

    Update RectancleUI.java
    Ajout de commentaires

--- Java1970/src/eu/epfc/java1970/lesson15/RectancleUI.java
+++ Java1970/src/eu/epfc/java1970/lesson15/RectancleUI.java
@@ -2,15 +2,25 @@ package eu.epfc.java1970.lesson15;
 
 import java.util.Scanner;
 
+/**
+ * classe qui gère l'interface utilisateur (UI : User Interface), en mode console
+ * pour le calcul de l'aire et du périmètre d'un rectangle
+ */
 public class RectancleUI {
+    
+    /**
+     * point d'entrée du programme
+     * invite l'utilisateur à introduire la hauteur et la largeur du Rectangle
+     * et retourne l'aire et le périmètre
+     */
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Rectangle rect = new Rectangle();
-        System.out.print("Hauteur: ");
-        rect.height = input.nextDouble();
-        System.out.print("Largeur: ");
-        rect.width = input.nextDouble();
-        System.out.println("Aire: " + rect.getArea());
-        System.out.println("Perimetre: " + rect.getPerimeter());
+        Scanner input = new Scanner(System.in);     // crée un objet de type Scanner connecté au clavier (System.in)
+        Rectangle rect = new Rectangle();           // crée un nouveau Rectangle
+        System.out.print("Hauteur: ");              // demande la hauteur à l'utilisateur
+        rect.height = input.nextDouble();           // sauve la saisie de l'utilisateur dans l'objet rect
+        System.out.print("Largeur: ");              // demande la largeur à l'utilisateur
+        rect.width = input.nextDouble();            // sauve la saisie de l'utilisateur dans l'objet rect
+        System.out.println("Aire: " + rect.getArea());  // affiche l'aire
+        System.out.println("Perimetre: " + rect.getPerimeter());    // affiche le périmètre
     }
 }
