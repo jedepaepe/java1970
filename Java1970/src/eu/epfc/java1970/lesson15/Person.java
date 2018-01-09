d20c0e0698b45ee5dd14c93b71bdec81d26357bb
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2018-01-09 22:05:32 +0100

    Update Person.java
    Ajout de commentaires

--- Java1970/src/eu/epfc/java1970/lesson15/Person.java
+++ Java1970/src/eu/epfc/java1970/lesson15/Person.java
@@ -1,13 +1,24 @@
 package eu.epfc.java1970.lesson15;
 
+/**
+ * Example de classe pour modéliser une Personne
+ */
 public class Person {
-    String firstName;
-    String familyName;
+    String firstName;   // attribut prénom
+    String familyName;  // attribut nom de famille
     
+    /**
+     * retourne le nom complet de la personne
+     */
     String getFullName() {
         return firstName + " " + familyName;
     }
     
+    /**
+     * code de test
+     * ce code devrait être dans une autre classe, DemoPerson par exemple, car il n'est pas bon de mélanger le code business et
+     *  le code de test
+     */
     public static void main(String[] args) {
         Person jmd = new Person();
         jmd.firstName = "Jean-Michel";
