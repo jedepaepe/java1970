a0c1b4842027a17de001c76e9916bcd80f986409
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2018-01-09 22:15:10 +0100

    Update RectangleBatch.java
    Ajout de commentaire

--- Java1970/src/eu/epfc/java1970/lesson15/RectangleBatch.java
+++ Java1970/src/eu/epfc/java1970/lesson15/RectangleBatch.java
@@ -1,5 +1,12 @@
 package eu.epfc.java1970.lesson15;
 
+/**
+ * Classe pour illustrer l'utilisateur de la classe Rectangle
+ * c'est la version "ligne de commande" de RectangleUI
+ * la hauteur et la largeur sont lues à partir de la ligne de commande:
+ *      java RectangleBatch 10 2
+ * affiche l'aire et le périmètre d'un rectangle de 10 (hauteur) sur 2 (largeur)
+ */
 public class RectangleBatch {
     public static void main(String[] args) {        
         Rectangle rect = new Rectangle();
@@ -8,8 +15,12 @@ public class RectangleBatch {
         System.out.println("Aire: " + rect.getArea());
         System.out.println("Perimetre: " + rect.getPerimeter());
         
+        /////////////////////////////
+        
+        // exemple d'utilisation du constructeur Rectangle(String[] args)
         rect = new Rectangle(args);
         
+        // exemple d'utilisation du constructeur Rectangle(double[] input)
         double[] input = new double[2];
         input[0] = 10;
         input[1] = 10;
