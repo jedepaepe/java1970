0f7c5bfcfc42334f6afadb00f149a671ed4af4b9
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2018-01-09 22:06:35 +0100

    Update Task.java
    Ajout de commentaires

--- Java1970/src/eu/epfc/java1970/lesson15/Task.java
+++ Java1970/src/eu/epfc/java1970/lesson15/Task.java
@@ -2,8 +2,12 @@ package eu.epfc.java1970.lesson15;
 
 import java.util.Date;
 
+/**
+ * Exemple de classe
+ * Classe pour gérer des tâches
+ */
 public class Task {
-    String name;
-    String description;
-    Date endDate;
+    String name;            // nom de la tâche
+    String description;     // sa description
+    Date endDate;           // date de fin de tâche
 }
