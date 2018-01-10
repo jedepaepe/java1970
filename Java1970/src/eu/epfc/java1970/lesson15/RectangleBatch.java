534ea30524a398997903855fc1a457f6c6eed43f
Jean-Michel Depaepe <jedepaepe@epfc.eu>
2018-01-10 07:22:02 +0100

    Update RectangleBatch.java
    Ajout de commentaires

--- Java1970/src/eu/epfc/java1970/lesson15/RectangleBatch.java
+++ Java1970/src/eu/epfc/java1970/lesson15/RectangleBatch.java
@@ -9,11 +9,19 @@ package eu.epfc.java1970.lesson15;
  */
 public class RectangleBatch {
     public static void main(String[] args) {        
-        Rectangle rect = new Rectangle();
-        rect.height = Double.valueOf(args[0]);
-        rect.width = Double.valueOf(args[1]);
-        System.out.println("Aire: " + rect.getArea());
-        System.out.println("Perimetre: " + rect.getPerimeter());
+        Rectangle rect = new Rectangle();       // crée l'objet de type Rectangle, la variable rect contient la référence vers cet objet
+        rect.height = Double.valueOf(args[0]);  // args[0] contient le premier argument de la ligne de commande, par ex: java eu.epfc.java1970.lesson15.Rectangle 1.0 2.0
+                                                //  c'est un objet de type String, il ne peut pas être assigné à la rect.height qui est un double
+                                                //  Double.valueOf("1.0") convertit la String "1.0" en un double : 1.0
+                                                //  rect.height = 1.0 assigne la valeur 1.0 à l'attribut "height" de rect
+                                                //    (plus exactement l'attribut de l'objet de type Rectangle référencé par la variable "height")
+        rect.width = Double.valueOf(args[1]);   // idem avec le deuxième argument de la ligne de commande
+        System.out.println("Aire: " + rect.getArea());  // affiche l'aire
+                                                        // rect.getArea() invoque la méthode getArea sur l'objet "rect"
+                                                        //      (plus exactement sur l'objet référencé par la variable "rect"
+                                                        // qui retourne l'aire du rectangle
+        System.out.println("Perimetre: " + rect.getPerimeter());    // rect.getPerimeter() invoque la méthode getPerimeter sur l'objet "rect"
+                                                                    // qui retourne le périmètre du rectangle
         
         /////////////////////////////
         
