4ec1d3386f0f41612adc1e95a3afd78492d1c2a1
FHA576 <jean-michel.depaepe@engie.com>
2018-01-16 22:04:00 +0100

    House démo

--- Java1970/src/eu/epfc/java1970/lesson16/house/House.java
+++ Java1970/src/eu/epfc/java1970/lesson16/house/House.java
@@ -3,7 +3,16 @@ package eu.epfc.java1970.lesson16.house;
 public class House {
     public String address;
     public Room[] roomList;
+
+    public House(String address) {
+        this.address = address;
+    }    
+    
     public double getArea() {
-        return 1;
+        double area = 0;
+        for (Room room : roomList) {
+            area += room.getArea();
+        }
+        return area;
     }
 }
--- Java1970/src/eu/epfc/java1970/lesson16/house/Room.java
+++ Java1970/src/eu/epfc/java1970/lesson16/house/Room.java
@@ -4,6 +4,13 @@ public class Room {
     public String name;
     public double length;
     public double width;
+
+    public Room(String name, double length, double width) {
+        this.name = name;
+        this.length = length;
+        this.width = width;
+    }
+    
     public double getArea() {
         return length * width;
     }
