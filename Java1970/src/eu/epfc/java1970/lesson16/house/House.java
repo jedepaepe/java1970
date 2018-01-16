e4200adba55107a76b39e4d14f97c33f29aa106c
FHA576 <jean-michel.depaepe@engie.com>
2018-01-16 22:22:56 +0100

    Ajout de commentaires

--- Java1970/src/eu/epfc/java1970/lesson16/house/House.java
+++ Java1970/src/eu/epfc/java1970/lesson16/house/House.java
@@ -1,13 +1,21 @@
 package eu.epfc.java1970.lesson16.house;
 
 public class House {
-    public String address;
-    public Room[] roomList;
+    public String address;      // adresse de la maison
+    public Room[] roomList;     // liste de pièce
 
+    /**
+     * Constructeur de la maison
+     * @param address : adresse de la maison
+     */
     public House(String address) {
         this.address = address;
     }    
     
+    /**
+     * 
+     * @return l'aire de la maison
+     */
     public double getArea() {
         double area = 0;
         for (Room room : roomList) {
--- Java1970/src/eu/epfc/java1970/lesson16/house/Room.java
+++ Java1970/src/eu/epfc/java1970/lesson16/house/Room.java
@@ -1,16 +1,26 @@
 package eu.epfc.java1970.lesson16.house;
 
 public class Room {
-    public String name;
-    public double length;
-    public double width;
+    public String name;     // nom de la pièce
+    public double length;   // longueur de la pièce
+    public double width;    // largeur de la pièce
 
+    /**
+     * Constructeur de la pièce
+     * @param name : nom de la pièce
+     * @param length : longueur de la pièce
+     * @param width : largeur de la pièce
+     */
     public Room(String name, double length, double width) {
         this.name = name;
         this.length = length;
         this.width = width;
     }
     
+    /**
+     * 
+     * @return l'aire de la pièce
+     */
     public double getArea() {
         return length * width;
     }
