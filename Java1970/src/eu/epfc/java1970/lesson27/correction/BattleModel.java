f9a29da3fe7f449de0145cc00aebe93f5a96c75d
FHA576 <jean-michel.depaepe@engie.com>
2018-04-25 21:44:40 +0200

    Ajout de la gestion de l'exception + commentaires pédagogiques

--- Java1970/src/eu/epfc/java1970/lesson27/correction/BattleModel.java
+++ Java1970/src/eu/epfc/java1970/lesson27/correction/BattleModel.java
@@ -29,19 +29,40 @@ public class BattleModel {
      * @throws IllegalArgumentException if size not valid (see above) 
      */
     public BattleModel(int size) {
+        if(size < 1 || size > 100) {    // si la taille de la grille (size) est incorrecte
+            throw new IllegalArgumentException("Invalid size " + size + " must be between 1 & 100");    // jette une exception (=> l'objet :BattleModel n'est pas créé)
+        }
+        // la taille est correcte => initialise l'objet :BattleModel
         this.size = size;
         friend = new FriendGrid(size);
         enemi = new EnemiGrid(size);
     }
 
+    /**
+     * getter state
+     * @return state
+     */
     public State getState() {
         return state;
     }
 
+    /**
+     * create a ship in the friend grid
+     * @param size : size of the ship
+     * @param orientation : orientation of the ship
+     * @param x : x position
+     * @param y : y position
+     */
     public void createShip(int size, Ship.Orientation orientation, int x, int y) {
         friend.createShip(size, orientation, x, y);
     }
 
+    /**
+     * fire the enemi
+     * @param x : x position
+     * @param y : y position
+     * @return a FireResult
+     */
     public FireResult fire(int x, int y) {
         FireResult result = enemi.bombed(x, y);
         // fire friend
@@ -50,14 +71,26 @@ public class BattleModel {
         return result;
     }
     
+    /**
+     * get the state of the cells of the enemi grid
+     * @return a matrix of CellState
+     */
     public CellState[][] getEnemiCellsState() {
         return enemi.getCellsState();
     }
     
+    /**
+     * get the state of the cells of the friend grid
+     * @return a matric of CellState
+     */
     public CellState[][] getFriendCellsState() {
         return friend.getCellsState();
     }
     
+    /**
+     * start the game
+     * @throws 
+     */
     public void setPlay() {
         state = State.PLAYING;
         ArrayList<Ship> friendShips = friend.getShips();
--- Java1970/src/eu/epfc/java1970/lesson27/correction/TestBattleModel.java
+++ Java1970/src/eu/epfc/java1970/lesson27/correction/TestBattleModel.java
@@ -7,35 +7,40 @@ package eu.epfc.java1970.lesson27.correction;
  * point d'entrée du programme (de test)
  */
 public class TestBattleModel {
+
     public static void main(String[] args) {
-        BattleModel battleModel = new BattleModel(5);
-        battleModel.createShip(1, Ship.Orientation.HORIZONTAL, 0, 0);
-        battleModel.createShip(1, Ship.Orientation.HORIZONTAL, 2, 2);
-        battleModel.createShip(1, Ship.Orientation.HORIZONTAL, 4, 4);
-        battleModel.setPlay();
-        
-        System.out.println("Fire 0,0");
-        battleModel.fire(0, 0);
-        System.out.println("Done? " + battleModel.isDone());
-        System.out.println("Enemi");
-        showCells(battleModel.getEnemiCellsState());
-        System.out.println("Friend");
-        showCells(battleModel.getFriendCellsState());
+        try {   // essaie de "jouer"
+            BattleModel battleModel = new BattleModel(5);
+            battleModel.createShip(1, Ship.Orientation.HORIZONTAL, 0, 0);
+            battleModel.createShip(1, Ship.Orientation.HORIZONTAL, 2, 2);
+            battleModel.createShip(1, Ship.Orientation.HORIZONTAL, 4, 4);
+            battleModel.setPlay();
+
+            System.out.println("Fire 0,0");
+            battleModel.fire(0, 0);
+            System.out.println("Done? " + battleModel.isDone());
+            System.out.println("Enemi");
+            showCells(battleModel.getEnemiCellsState());
+            System.out.println("Friend");
+            showCells(battleModel.getFriendCellsState());
+        } catch (IllegalArgumentException ex) { // une exception s'est produite
+            System.out.println("Erreur:\n\t" + ex.getMessage() + "\nImpossible de jouer, sorry");
+        }
     }
-    
+
     private static void showCells(CellState[][] cells) {
         StringBuilder sb = new StringBuilder();
-        for(int l=0; l<cells.length; l++) {
-            for(int c=0; c<cells[l].length; c++) {
+        for (int l = 0; l < cells.length; l++) {
+            for (int c = 0; c < cells[l].length; c++) {
                 sb.append(convertCellState(cells[l][c]));
             }
             sb.append("\n");
         }
         System.out.println(sb.toString());
     }
-    
+
     private static char convertCellState(CellState cellState) {
-        switch(cellState) {
+        switch (cellState) {
             case BOMB_WATER:
                 return 'B';
             case SHIP:
